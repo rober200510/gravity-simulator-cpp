3299f70e9e4170cf4e927d2d4dc7ec9b8964773c
rober200510 <167541732+rober200510@users.noreply.github.com>
2026-01-24 12:16:46 +0100

    refactor: replace individual body variables with std::vector system

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <SFML/Graphics.hpp>
 #include "Body.hpp"
+#include <vector>
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Gravity Simulator");
     window.setFramerateLimit(60);
 
     // --- INITIALIZATION ---
-    Body planet(400.0f, 300.0f, 20.0f); 
+    std::vector<Body> bodies;
+    bodies.push_back(Body(400, 300, 100));
 
-    // Give it an initial push to the right (50 pixels per second)
-    planet.velocity.x = 50.0f; 
+    bodies.push_back(Body(200, 300, 20));
+    bodies[1].velocity.y = 50.0f;
+
+    bodies.push_back(Body(600, 300, 20));
+    bodies[2].velocity.y = -50.0f;
 
     sf::Clock clock; //For dt
 
@@ -22,11 +27,16 @@ int main() {
         //update logic
         sf::Time elapsed = clock.restart();
         float dt = elapsed.asSeconds();
-        planet.update(dt);
+        for (auto& body : bodies) {
+            body.update(dt);
+        }
 
         // --- DRAW ---
         window.clear(sf::Color::Black);
-        planet.draw(window);
+
+        for (auto& body : bodies) {
+            body.draw(window);
+        }
         window.display();
     }
     return 0;
